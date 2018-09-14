12baa502d2905f229116faa598e5c8a92756d4fe
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-09-13 22:04:30 -0500

    restructure and first endpoint works

--- groupme-messages/handler.js
+++ groupme-messages/handler.js
@@ -1,14 +1,53 @@
 'use strict';
 
-module.exports.hello = async (event, context) => {
-  return {
+/* handler.js */
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql');
+
+// This method just inserts the user's first name into the greeting message.
+const getGreeting = firstName => `Hello, ${firstName}.`;
+
+// Here we declare the schema and resolvers for the query
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType', // an arbitrary name
+    fields: {
+      // the query has a field called 'greeting'
+      greeting: {
+        // we need to know the user's name to greet them
+        args: { firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) } },
+        // the greeting message is a string
+        type: GraphQLString,
+        // resolve to a greeting message
+        resolve: (parent, args) => getGreeting(args.firstName)
+      }
+    }
+  }),
+});
+
+// We want to make a GET request with ?query=<graphql query>
+// The event properties are specific to AWS. Other providers will differ.
+module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
+  .then(
+    result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+    err => callback(err)
+  );
+
+module.exports.handleMessage = (event, context, callback) => {
+	console.log(event);
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    headers: {
+    	'Access-Control-Allow-Origin': '*',
+    	'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({ message: "Hello World!" }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
